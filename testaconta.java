d05bfdb3bfbd12d76b7a1a3a469e1acbf60fc47f
Flavio Alves <gospelmixba@gmail.com>
2021-11-22 21:48:27 -0300

    mudança de valores limites

--- testaconta.java
+++ testaconta.java
@@ -1,11 +1,11 @@
 public class testaconta {
     public static void main (String[] args){
-        Contacorrente cc1 = new Contacorrente(22,1,"Santander",100.00,100.00,125.50);
+        Contacorrente cc1 = new Contacorrente(22,1,"Santander",100.00,500.00,600.00);
         System.out.println(cc1);
 
 
         System.out.println("o saldo da conta corrente é R$ " +cc1.getSaldo  ());
-        System.out.println( "Seus limite de saque é " + cc1.saldo + cc1.chequeEspecial );
+
 
 
 
@@ -18,7 +18,7 @@ public class testaconta {
 
         ContaSalario S1 =new ContaSalario(21,25,"Mentorama",5000.00,4,500.00,0);
 
-        System.out.println(" Seu limite na conta Salario é R$: "  +  S1.getSaldo());
+        System.out.println("Seu limite na conta Salario é R$: "  +  S1.getSaldo());
 
 
 
